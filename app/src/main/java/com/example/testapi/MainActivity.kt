79f2585196a6113eaf26ef76e354803a5800b2c5
Artem Tihonov <iamtihonov@gmail.com>
2020-05-27 13:07:11 +0300

    Добавленн адрес

--- app/src/main/java/com/example/testapi/MainActivity.kt
+++ app/src/main/java/com/example/testapi/MainActivity.kt
@@ -1,3 +1,5 @@
+@file:Suppress("unused")
+
 package com.example.testapi
 
 import android.os.Bundle
@@ -8,17 +10,20 @@ import java.lang.Exception
 import java.net.URL
 import javax.net.ssl.HttpsURLConnection
 
-/**
- * При первом запуске после установки, выдает FileNotFoundException
- */
 class MainActivity : AppCompatActivity() {
 
     private lateinit var url: URL
 
+    //Оба работают
+    private val url1 = "https://testtykhonov.000webhostapp.com/test.json"
+    private val url2 = "https://dev-api.raddy.me/api/v1/app/onboarding"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        url = URL("https://testtykhonov.000webhostapp.com/test.json")
+
+
+        url = URL(url2)
 
         Log.e("testLog", "MainActivity onCreate()")
         LoadingThread().start()
